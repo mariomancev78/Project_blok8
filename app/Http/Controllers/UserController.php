81b65757caa570054fc13e44ab9cfe2d528e2475
Mario Mancev <mariomancev78@gmail.com>
2023-06-08 16:45:24 +0200

    eerste commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        $users = User::all();
+
+        return view('users.index', ['users' => $users]);
+    }
+
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'email' => 'required|unique:users|max:255',
+            'lastname' => 'required'
+        ]);
+
+
+        $user = new User();
+        $user->firstname = $request->firstname;
+        $user->lastname = $request->lastname;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+
+        return redirect()->route('overzicht-gebruikers');
+    }
+}
