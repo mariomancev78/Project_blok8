9beaab860bf025788f8b59440dfe2723c7ea7068
Mario Mancev <mariomancev78@gmail.com>
2023-06-15 17:30:23 +0200

    login en registreren werken nu allebei, de indeling van de views is alleen nog niet logisch, :/ nu alleen nog zorgen dat de naam van de gebruiker getoond wordt tijdens het aanmaken van een question.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,23 +7,23 @@
 
     <div class="container">
         <div class="formContainer">
-            
-            @if (session()->has('message'))
-            <div class="alert">
-                {{ session('message') }}
-            </div>
-            @endif
-
             <h2>Stel een vraag</h2>
+
+            @if (session()->has('LoggedUser'))
+
             <form action="{{ route('CreateQuestion') }}" method="POST">
                 {{ csrf_field() }}
-                <input type="text" name="name" id="name" placeholder="naam">
                 <input type="text" name="title" id="title" placeholder="titel">
                 <input type="text" name="description" id="description" placeholder="beschrijving">
                 <textarea name="body" id="body" cols="30" rows="10"> </textarea>
                 <input type="submit" value="Submit">
             </form>
-            <p>nog geen account? <a href="{{ route('account') }}">maak er een aan</a> </p>
+            @else
+            <p>Om een vraag te stellen moet je eerst inloggen</p>
+            <p>Nog geen account? <a href="{{ route('account') }}">maak er een aan</a> </p>
+            <p>Al in het bezit van een account? <a href="{{ route('account') }}">log in</a> </p>
+            @endif
+           
         </div>
     </div>
 @endsection
